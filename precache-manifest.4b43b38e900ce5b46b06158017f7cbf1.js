333d69d899284ec9bbea902a4af5f419d7dda00d
josue rojas <josuerojas.rojas@gmail.com>
2020-02-18 21:39:55 -0600

    Updates

---
+++ precache-manifest.4b43b38e900ce5b46b06158017f7cbf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafa0ab65c1fa6c619ffff017eeb6dc6",
+    "url": "/SunMoonReact/index.html"
+  },
+  {
+    "revision": "5a48bdadddd4ebde61d9",
+    "url": "/SunMoonReact/static/css/main.0d794a40.chunk.css"
+  },
+  {
+    "revision": "c38fabfad7616e53389b",
+    "url": "/SunMoonReact/static/js/2.431551bc.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/SunMoonReact/static/js/2.431551bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a48bdadddd4ebde61d9",
+    "url": "/SunMoonReact/static/js/main.ddff7acd.chunk.js"
+  },
+  {
+    "revision": "89387f8a40bf4c460049",
+    "url": "/SunMoonReact/static/js/runtime-main.4023d23b.js"
+  }
+]);
